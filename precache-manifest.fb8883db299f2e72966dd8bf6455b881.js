ff8bc70206ae4c528a16a26ad66ed4f459f29be0
appledrft2 <ragiedoromal06@gmail.com>
2020-02-25 21:18:28 +0800

    Updates

---
+++ precache-manifest.fb8883db299f2e72966dd8bf6455b881.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6644acf7ee28efba69838f6e3d9c30c1",
+    "url": "/todoreactapp/index.html"
+  },
+  {
+    "revision": "29bb1fa246c576654bb9",
+    "url": "/todoreactapp/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "b609ff9b8a1a7fa64d39",
+    "url": "/todoreactapp/static/css/main.61380335.chunk.css"
+  },
+  {
+    "revision": "29bb1fa246c576654bb9",
+    "url": "/todoreactapp/static/js/2.8bdfee27.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/todoreactapp/static/js/2.8bdfee27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b609ff9b8a1a7fa64d39",
+    "url": "/todoreactapp/static/js/main.8a1f112a.chunk.js"
+  },
+  {
+    "revision": "ebed2a01c035555b7c82",
+    "url": "/todoreactapp/static/js/runtime-main.593aa650.js"
+  }
+]);
